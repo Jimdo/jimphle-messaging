59c0dd778f8818f60853880c7ca0777be1aa9785
Joscha <joscha@schnipseljagd.org>
2015-06-22 11:56:18 +0200

    add optional param priority

--- src/Jimphle/Messaging/AbstractMessageHandler.php
+++ src/Jimphle/Messaging/AbstractMessageHandler.php
@@ -9,22 +9,24 @@ abstract class AbstractMessageHandler implements MessageHandler
      * @param string $name
      * @param array $payload
      * @param null|string $channel
+     * @param null|int $priority
      * @return \Jimphle\Messaging\Event
      */
-    protected function event($name, array $payload, $channel = null)
+    protected function event($name, array $payload, $channel = null, $priority = null)
     {
-        return Event::generate($name, $payload, $channel);
+        return Event::generate($name, $payload, $channel, $priority);
     }
 
     /**
      * @param string $name
      * @param array $payload
      * @param null|string $channel
+     * @param null|int $priority
      * @return \Jimphle\Messaging\Command
      */
-    protected function command($name, array $payload, $channel = null)
+    protected function command($name, array $payload, $channel = null, $priority = null)
     {
-        return Command::generate($name, $payload, $channel);
+        return Command::generate($name, $payload, $channel, $priority);
     }
 
     /**
